c1cfdbce037f325e163b5fb9686831ada3abf000
Christian Rhodes <thechristianrhodes@gmail.com>
2023-11-12 20:01:34 -0700

    add initial content to games

---
+++ src/pages/wheeloffortune/play/index.tsx
@@ -0,0 +1,26 @@
+import Page from '@/components/page';
+import ActionButtonItem from '../../../components/buttons/ActionButton';
+import {useSidebar} from '@/contexts/SidebarContext';
+
+export default function PlayWheelOfFortune() {
+  const sidebarStateActive = useSidebar();
+
+  return (
+    <Page showConnectButton={true} showNav={false} showAppFooter={false} showAppHeader={false}>
+      <div
+        className={`${sidebarStateActive ? 'sidebarActive' : 'transition-all duration-300'} min-h-screen gameBGImage`}
+      >
+        <div className="mt-24 mx-[4.5rem]">
+          <h4 className="text-brand-green mb-32">Play Wheel of Fortune</h4>
+          <section
+            className={`${
+              sidebarStateActive ? 'contentContainerWithSidebarNoBG' : 'contentContainerWithoutSidebarNoBG'
+            } mb-24`}
+          >
+            <p>game</p>
+          </section>
+        </div>
+      </div>
+    </Page>
+  );
+}
