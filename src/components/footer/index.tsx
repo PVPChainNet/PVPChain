e06589f99c6fb5ab6ef68b5f9aeaea41f8add166
Christian Rhodes <thechristianrhodes@gmail.com>
2023-11-28 16:57:24 -0700

    update footer to reduce visual clutter

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -7,42 +7,46 @@ export default function Nav() {
 
   return (
     <nav className="w-full bg-slate-light flex flex-col justify-center align-middle">
-      <Link className="my-8 mx-auto" href="/">
+      <Link className="my-4 mx-auto sm:ml-8" href="/">
         <Image src="/images/logos/pvp_logo.svg" width={100} height={100} alt="logo" />
       </Link>
-      <div className="flex flex-row justify-between gap-8 max-w-[450px] mx-auto">
-        <Link href="/profile">
-          <p>Profile</p>
-        </Link>
-        <Link href="/tokenleaderboard">
-          <p>Token Leaderboard</p>
-        </Link>
-        <Link href="/playerleaderboard">
-          <p>Player Leaderboard</p>
-        </Link>
-      </div>
-      <div className="flex flex-row justify-between gap-8 mt-4 max-w-[450px] mx-auto">
-        {menuItemsPVP.map((item, index) => (
-          <Link key={index} href={item.link}>
-            <p>{item.name}</p>
+      <div className="flex flex-col sm:flex-row justify-evenly gap-8 mb-8">
+        <div className="flex flex-col justify-center items-center text-center">
+          <Link href="/profile">
+            <p>Profile</p>
           </Link>
-        ))}
+          <Link href="/tokenleaderboard">
+            <p>Token Leaderboard</p>
+          </Link>
+          <Link href="/playerleaderboard">
+            <p>Player Leaderboard</p>
+          </Link>
+        </div>
+        <div className="flex flex-col justify-center items-center text-center">
+          {menuItemsPVP.map((item, index) => (
+            <Link key={index} href={item.link}>
+              <p>{item.name}</p>
+            </Link>
+          ))}
+        </div>
       </div>
-      <div className="flex flex-row justify-between gap-8 mt-4 max-w-[450px] mx-auto">
-        <Link href="/#">
-          <p>Whitepaper</p>
-        </Link>
-        <Link href="/#">
-          <p>Privacy Policy</p>
-        </Link>
-      </div>
-      <div className="flex flex-row justify-between gap-8 mt-8 max-w-[450px] mx-auto">
-        <Image src={'/images/socials/twitter.svg'} width={30} height={30} alt={'twitter icon'} />
-        <Image src={'/images/socials/discord.svg'} width={30} height={30} alt={'discord icon'} />
-        <Image src={'/images/socials/telegram.svg'} width={30} height={30} alt={'telegram icon'} />
-      </div>
-      <div className="flex flex-row justify-between gap-8 mt-8 mb-4 max-w-[450px] mx-auto">
-        <p>{`PvP @${year}, All Rights Reserved`}</p>
+      <div className="w-full bg-black-main flex flex-col md:flex-row justify-evenly items-center pb-4">
+        <div className="flex flex-col sm:flex-row justify-center text-center sm:gap-4 mt-4 max-w-[450px] mx-auto">
+          <Link href="https://app.gitbook.com/o/bosh1Lw7viN5cWrdY2Ig/s/NzQVjNo6xDa3gVEMSXEb/" target="_blank">
+            <p>Whitepaper</p>
+          </Link>
+          <Link href="/#">
+            <p>Privacy Policy</p>
+          </Link>
+        </div>
+        <div className="flex flex-row justify-between gap-8 mt-8 mb-4 max-w-[450px] mx-auto">
+          <p>{`PvP @${year}, All Rights Reserved`}</p>
+        </div>
+        <div className="flex flex-row justify-between gap-8 mt-8 max-w-[450px] mx-auto">
+          <Image src={'/images/socials/twitter.svg'} width={30} height={30} alt={'twitter icon'} />
+          <Image src={'/images/socials/discord.svg'} width={30} height={30} alt={'discord icon'} />
+          <Image src={'/images/socials/telegram.svg'} width={30} height={30} alt={'telegram icon'} />
+        </div>
       </div>
     </nav>
   );
